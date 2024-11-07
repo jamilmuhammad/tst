c81f91d5cec6ce4f7191ad213a1291cc19cc10c8
Muhammad Jamil <mjamil1339@gmail.com>
2024-11-08 00:19:03 +0700

    fix: update cache redis

--- app/cache.py
+++ app/cache.py
@@ -1,12 +1,16 @@
 import redis
 import json
-from config import settings
+from .config import get_settings
+
+settings = get_settings()
 
 redis_client = redis.from_url(settings.REDIS_URL)
 
 def set_cache(key: str, value: dict, expires: int = 3600):
-    redis_client.setex(key, expires, json.dumps(value))
+    redis_client.setex(key, expires, json.dumps(value, default=str))
 
 def get_cache(key: str) -> dict:
     data = redis_client.get(key)
-    return json.loads(data) if data else None
+    if data:
+        return json.loads(data) 
+    return None
